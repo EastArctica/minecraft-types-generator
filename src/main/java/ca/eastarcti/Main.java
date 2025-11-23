c0a021c1b951b29486eafa5f7456fe8ef0d8cfec
Jack Manning <eastarctica@gmail.com>
2025-11-23 16:19:01 -0500

    Update generated readmes

--- src/main/java/ca/eastarcti/Main.java
+++ src/main/java/ca/eastarcti/Main.java
@@ -145,19 +145,113 @@ express Statement of Purpose.
  d. Affirmer understands and acknowledges that Creative Commons is not a
     party to this document and has no duty or obligation with respect to
     this CC0 or use of the Work.""";
+    // TODO: Show available builds in README
+    private static final String README_TEMPLATE = """
+# @minecraft-types/yarn-<MINECRAFT_VERSION>
+
+Typescript definitions for Minecraft <MINECRAFT_VERSION> (Fabric/Yarn mappings), all known builds.
+
+## Usage
+
+Install from npm or pnpm:
+
+```bash
+npm install @minecraft-types/yarn-<MINECRAFT_VERSION>
+# or
+pnpm add @minecraft-types/yarn-<MINECRAFT_VERSION>
+```
+
+Then, in your `tsconfig.json`, add the following to the `compilerOptions` section:
+
+```json
+{
+  "compilerOptions": {
+    "types": [
+      "@minecraft-types/yarn-<MINECRAFT_VERSION>"
+    ]
+  }
+}
+```
+
+If you require a specific yarn build number instead of the default (latest) build, specify it like this:
+```json
+{
+  "compilerOptions": {
+    "types": [
+      "@minecraft-types/yarn-<MINECRAFT_VERSION>/build.<BUILD_NUMBER>"
+    ]
+  }
+}
+```
+
+Replace `<BUILD_NUMBER>` with the desired build number.
+
+Note: adding the `types` array explicitly tells TypeScript which global type packages to include. When you set `compilerOptions.types` it will only include the packages listed there and will not automatically include other `@types/*` packages (for example `node`). If you need other ambient types, add them to the array as well or consider using `typeRoots` or a project `global.d.ts` instead.
+
+## Using the types and handling globals
+
+By default, this will expose the types under the `Packages` namespace (ex. `Packages.java.io.BufferedInputStream`).
+
+If you work in an environment where the global namespace is polluted differently with Java types, you can re-namespace the types by creating a `global.d.ts` file in your project with the following content:
+
+```ts
+// global.d.ts
+declare global {
+  const java: typeof Packages.java;
+  // Add other root namespaces as needed (e.g., javax, com, org, etc.)
+  // const com: typeof Packages.com;
+
+  // If your environment exposes something other than 'Packages', you can alias it here
+  // const SomeOtherPackages: typeof Packages;
+}
+
+export {}; // keep file a module so TS merges properly
+```
+
+If instead, you'd prefer to only pollute the global namespace within that file, you can add the following to your typescript files:
+```ts
+declare const java: typeof Packages.java;
+// Add other root namespaces as needed (e.g., javax, com, org, etc.)
+// declare const com: typeof Packages.com;
+
+// If your environment exposes something other than 'Packages', you can alias it here
+// declare const SomeOtherPackages: typeof Packages;
+```
+
+## Examples
+
+- Referencing a Java type directly from the ambient `Packages` types:
+
+```ts
+// Uses the package name as exposed by the types
+type BIS = Packages.java.io.BufferedInputStream;
+
+declare const inStream: BIS;
+```
+
+- Using the `java` alias from the `global.d.ts` example:
+
+```ts
+const s: string = java.lang.String.valueOf(123);
+```
+
+## License
+
+CC0-1.0
+""";
     private static final String PACKAGE_TEMPLATE = """
 {
   "name": "@minecraft-types/yarn-<MINECRAFT_VERSION>",
-  "version": "1.0.0",
+  "version": "1.0.3",
   "description": "Typescript definitions for Minecraft <MINECRAFT_VERSION> (Fabric/Yarn mappings), all known builds.",
-  "homepage": "https://github.com/East_Arctica/minecraft-types#readme",
+  "homepage": "https://github.com/EastArctica/minecraft-types#readme",
   "bugs": {
-    "url": "https://github.com/East_Arctica/minecraft-types/issues"
+    "url": "https://github.com/EastArctica/minecraft-types/issues"
   },
   "license": "CC0-1.0",
   "author": {
     "name": "East_Arctica",
-    "url": "https://github.com/East_Arctica"
+    "url": "https://github.com/EastArctica"
   },
   "files": [
     "README.md",
@@ -166,8 +260,8 @@ express Statement of Purpose.
   ],
   "repository": {
     "type": "git",
-    "url": "https://github.com/East_Arctica/minecrat-types.git",
-    "directory": "<MINECRAFT_VERSION>/"
+    "url": "https://github.com/EastArctica/minecraft-types.git",
+    "directory": "yarn-<MINECRAFT_VERSION>/"
   },
   "types": "build.<DEFAULT_BUILD_NUMBER>/index.d.ts"
 }""";
@@ -443,7 +537,7 @@ express Statement of Purpose.
     private static void processIntermediary(FabricIntermediaryData intermediary, FabricYarnData[] yarns) throws Exception {
         System.out.println("Intermediary version: " + intermediary.version);
 
-        String versionDir = Paths.get(VERSIONS_DIR, intermediary.version).toString();
+        String versionDir = Paths.get(VERSIONS_DIR, "yarn-" + intermediary.version).toString();
         try {
             Files.createDirectories(Paths.get(versionDir));
         } catch (IOException e) {
@@ -529,6 +623,17 @@ express Statement of Purpose.
             Files.writeString(packageJsonPath, packageJsonContent, StandardOpenOption.CREATE);
         } catch (IOException e) {
             System.err.println("  Error writing package.json file.");
+            throw new RuntimeException(e);
+        }
+
+        // Update README.md
+        Path readmePath = Paths.get(versionDir, "README.md");
+        String readmeContent = README_TEMPLATE
+                .replaceAll("<MINECRAFT_VERSION>", intermediary.version);
+        try {
+            Files.writeString(readmePath, readmeContent, StandardOpenOption.CREATE);
+        } catch (IOException e) {
+            System.err.println("  Error writing README.md file.");
             throw new RuntimeException(e);
         }
     }
